5bff7ce00120a1e4adec51419c2d96a62895a478
Raul Mag <61987994+magraul@users.noreply.github.com>
2024-05-19 01:42:22 +0300

    Security (#4)
    * Implement security
    
    * qq
    
    * Update build.gradle

--- document-e2e/src/main/java/com/xdisx/document/e2e/document/rest/DocumentController.java
+++ document-e2e/src/main/java/com/xdisx/document/e2e/document/rest/DocumentController.java
@@ -1,10 +1,12 @@
 package com.xdisx.document.e2e.document.rest;
 
 import com.xdisx.document.api.DocumentApi;
+import com.xdisx.document.e2e.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(
         contextId = "documentService",
         name = "${xdisx.service.document.client-name}",
-        url = "${xdisx.service.document.url}")
+        url = "${xdisx.service.document.url}",
+        configuration = FeignConfig.class)
 public interface DocumentController extends DocumentApi {}
